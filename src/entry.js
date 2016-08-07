712206ed171e43e9e99ffcc0aa0a7062b01b440d
DevinFrenze <frenze.devin@gmail.com>
2016-08-07 14:56:57 -0700

    successfully reduces proportions to simple whole number ratios

--- src/entry.js
+++ src/entry.js
@@ -1,35 +1,64 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {findRatio} from 'lib/index'
 import 'styles/main'
 import 'whatwg-fetch'
 
 class App extends React.Component {
   componentWillMount() {
-    this.state = { base: 'USD' }
+    this.state = {
+      currentBase: 'USD',
+      currentCompare: 'HUF',
+      ratios: {},
+      rates: {}
+    }
     this.fetchRates()
   }
 
-  async fetchRates() {
-    const { base } = this.state
-    const response = await fetch(`http://api.fixer.io/latest?base=${base}`)
-
-    if (response.status < 400) {
-      const rates = await response.json()
-      this.setState(Object.assign( this.state, rates ))
+  componentDidUpdate() {
+    const { currentCompare, ratios, rates } = this.state
+    if (!ratios[currentCompare] && rates) {
+      setTimeout( () => {
+        const ratio = findRatio(1, rates[currentCompare])
+        this.setState({ ratios: Object.assign( {}, this.state.ratios, { [currentCompare]: ratio }) })
+      }, 0)
     }
   }
 
-  render () {
+  async fetchRates() {
+    const { currentBase, currentCompare } = this.state
+    const response = await fetch(`http://api.fixer.io/latest?base=${currentBase}`)
 
-    const { base, rates } = this.state
+    if (response.status < 400) {
+      const results = await response.json()
+      this.setState({
+        options: Object.keys(results.rates).sort(),
+        rates: results.rates
+      })
+    }
+  }
+
+  changeCompareCurrency (indexShift) {
+    const { options, currentCompare, rates } = this.state
+    let newIndex = options.indexOf(currentCompare) + indexShift
+    if (newIndex >= options.length) newIndex -= options.length
+    else if (newIndex < 0) newIndex += options.length
+
+    const newCompare = options[newIndex]
+    this.setState({ currentCompare: newCompare })
+  }
+
+  render () {
+    const { currentBase, currentCompare, ratios, options } = this.state
+    const ratio = ratios[currentCompare]
+    console.log('render ' + currentCompare)
     return (
       <div>
         <div>REACT IS THERE</div>
-        <div>{base} = 1</div>
-        <div>{rates ?
-          rates.EUR && ('EUR = ' + rates.EUR) :
-          'Loading'
-        }</div>
+        <div>{currentBase} = {ratio ? ratio[0] : '?'}</div>
+        <div>{currentCompare} = {ratio ? ratio[1] : '?'}</div>
+        <div onClick={() => this.changeCompareCurrency(-1)}>back</div>
+        <div onClick={() => this.changeCompareCurrency(1)}>forward</div>
       </div>
     )
   }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,77 @@
+/*
 import 'whatwg-fetch'
 
 export async function getRate() {
   const response = await fetch('http://api.fixer.io/latest')
 
   return await response.json()
+}
+*/
+
+function primeFactorization(num) {
+  const results = { 1: num }
+  let n = num
+  for (var i = 2; i < Math.sqrt(num); i++) {
+    while (n % i === 0) {
+      if (!results[i]) results[i] = 0
+      results[i]++
+      n /= i
+    }
+  }
+  results[n] = 1
+  return results
+}
+
+function simplifyFactors(aFactors, bFactors) {
+  const aKeys = Object.keys(aFactors)
+  for (var i = 0; i < aKeys.length; i++) {
+    let key = aKeys[i]
+    while (aFactors[key] > 0 && bFactors[key] > 0) {
+      aFactors[key]--
+      bFactors[key]--
+    }
+  }
+}
+
+function numberFromFactors(factors) {
+  const aKeys = Object.keys(factors)
+  let num = 1
+  for (var i = 0; i < aKeys.length; i++) {
+    let key = aKeys[i]
+    while (factors[key] > 0) {
+      num *= key
+      factors[key]--
+    }
+  }
+  return num
+}
+
+function findExactRatio(a, b) {
+  let aFactors = primeFactorization(a), bFactors = primeFactorization(b)
+  simplifyFactors(aFactors, bFactors)
+  const result = [
+    numberFromFactors(aFactors),
+    numberFromFactors(bFactors)
+  ]
+  return result
+}
+
+function getSimplestRatio(ratios) {
+  let simplest = ratios[0]
+  for (let i = 1; i < ratios.length; i++) {
+    if (ratios[i][0] + ratios[i][1] < simplest[0] + simplest[1]) {
+      simplest = ratios[i]
+    }
+  }
+  return simplest
+}
+
+export function findRatio(num1, num2) {
+  const ratios = []
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      ratios.push(findExactRatio(Math.round(num1 * 100) + i, Math.round(num2 * 100) + j))
+    }
+  }
+  return getSimplestRatio(ratios)
 }
